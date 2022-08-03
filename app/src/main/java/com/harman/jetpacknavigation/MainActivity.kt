730ccf2d963d7dbc126bea77a06a8062e260ecfa
Harmanjot Kaur <109522011+Harmankjot18@users.noreply.github.com>
2022-08-03 06:55:19 +0530

    fragment

---
+++ app/src/main/java/com/harman/jetpacknavigation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.harman.jetpacknavigation
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       navController =findNavController(R.id.navHost)
+    }
+
+}
